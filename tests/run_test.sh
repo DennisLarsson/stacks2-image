c5b8c3e473b4da70a8ebf16b2dd8afd1e561765f
DennisLarsson <dennis.j.larsson@gmail.com>
2024-06-12 14:56:01 +0200

    Add to output parameter values

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -3,3 +3,5 @@
 ./parameter_optimization.py --popmap popmap_test --samples /test_samples/ --min_val 1 --max_val 3
 
 diff best_params.txt expected_best_params.txt
+cat param_vals_nm.txt
+cat param_vals_n.txt
