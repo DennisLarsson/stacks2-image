c95c2f83cf5cbeb1372678439c76421ee3f203fd
DennisLarsson <dennis.j.larsson@gmail.com>
2024-06-12 11:03:50 +0200

    Add samples path and popmap path to denovo_map.pl run commands and allow cpu to be manually defined

--- parameter_optimization.py
+++ parameter_optimization.py
@@ -20,7 +20,7 @@ def grep(file_path, search_text):
 def create_directories(dir_path):
     os.makedirs(dir_path)
 
-def run_denovo_map_nm(n_val, m_val, cpu_count, popmap_path):
+def run_denovo_map_nm(n_val, m_val, cpu_count, samples_path, popmap_path):
     stacks_folder, pop_folder = create_folder_names(n_val, m_val)
     create_directories(stacks_folder)
     create_directories(pop_folder)
@@ -29,7 +29,9 @@ def run_denovo_map_nm(n_val, m_val, cpu_count, popmap_path):
                     "-n", n_val, 
                     "-M", m_val, 
                     "-T", cpu_count, 
-                    "-o", stacks_folder], 
+                    "-o", stacks_folder, 
+                    "--samples", samples_path,
+                    "--popmap", popmap_path],
                     check=True)
     
     subprocess.run(["populations", 
@@ -37,7 +39,8 @@ def run_denovo_map_nm(n_val, m_val, cpu_count, popmap_path):
                     "--in_path", stacks_folder, 
                     "--out_path", pop_folder, 
                     "--popmap", popmap_path, 
-                    "-R", "0.8"], check=True)
+                    "-R", "0.8"], 
+                    check=True)
     
     R80_val = check_R80_val(n_val, m_val)  
     return R80_val
@@ -55,11 +58,11 @@ def run_optimization(cpu, args, val_m=None):
     results = {}
     for val in range(args.min_val, args.max_val + 1):
         if val_m is None:
-            R80_val = run_denovo_map_nm(val, val, cpu, args.popmap)
+            R80_val = run_denovo_map_nm(val, val, cpu, args.samples, args.popmap)
         else:
             if val == val_m:
                 continue
-            R80_val = run_denovo_map_nm(val, val_m, cpu, args.popmap)
+            R80_val = run_denovo_map_nm(val, val_m, cpu, args.samples, args.popmap)
         
         results[val] = int(R80_val)
     return results
@@ -67,11 +70,16 @@ def run_optimization(cpu, args, val_m=None):
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(description="Run denovo_map.pl and populations for different n and m values")
     argparser.add_argument("--popmap", help="Path to the population map file")
+    argparser.add_argument("--samples", help="Path to the samples file")
+    argparser.add_argument("--cpu", type=int, help="Number of CPUs to use", default="1")
     argparser.add_argument("--min_val", type=int, help="Minimum value for n and m", default=1)
     argparser.add_argument("--max_val", type=int, help="Maximum value for n and m", default=7)
     args = argparser.parse_args()
 
-    cpu_count = multiprocessing.cpu_count()
+    if args.cpu:
+        cpu_count = args.cpu
+    else:
+        cpu_count = multiprocessing.cpu_count()
 
     results = run_optimization(cpu_count, args, val_m=None)
     best_val_m = find_best_val(results)
